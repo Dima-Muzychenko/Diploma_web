49b166b06bb92817c7934b34bf292a1193bc601a
Dima Muzychenko <robin700521@gmail.com>
2023-05-24 02:43:33 +0300

    fuzzy logic

--- src/main/java/FuzzyLogic/ServiceStationAttractiveness.java
+++ src/main/java/FuzzyLogic/ServiceStationAttractiveness.java
@@ -5,6 +5,11 @@ import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.RuleBlock;
 
 public class ServiceStationAttractiveness {
+
+    public static double Bell(double one, double tow, double thee){
+
+        return 1;
+    }
     public static void main(String[] args) {
         // Load FCL file
         String fileName = "src/main/java/FuzzyLogic/service_station_attractiveness.fcl";
@@ -16,11 +21,12 @@ public class ServiceStationAttractiveness {
             return;
         }
 
+
         // Set inputs
-        fis.setVariable("quality", 2.0);
-        fis.setVariable("speed", 1.7);
-        fis.setVariable("price", 8.3);
-        fis.setVariable("service_range", 1.2);
+        fis.setVariable("quality", 8.2);
+        fis.setVariable("speed", 8.7);
+        fis.setVariable("price", 8.9);
+        fis.setVariable("service_range", 8.45);
 
         // Evaluate
         fis.evaluate();
@@ -35,4 +41,5 @@ public class ServiceStationAttractiveness {
         RuleBlock ruleBlock = fis.getFunctionBlock("service_station_attractiveness").getFuzzyRuleBlock("No1");
         JFuzzyChart.get().chart(ruleBlock.getFunctionBlock());
     }
+
 }
